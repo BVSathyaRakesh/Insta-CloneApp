3453cc647cf8d17ca74f8bb26a0737fc059c3f6b
Sathya Rakesh <bvsrakesh.kumar@i-exceed.com>
2022-11-01 20:46:55 +0530

    Initial Commit

---
+++ Insta-CloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insta-CloneApp
+//
+//  Created by Rakesh BVS. Kumar on 2022/11/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
